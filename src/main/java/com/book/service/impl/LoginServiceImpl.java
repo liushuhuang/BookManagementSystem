ed7f15993414ef233e51366fe17bcdf4d6306969
lambda <21..787301@qq.com>
2026-06-15 16:06:05 +0800

    feat-登陆功能

--- src/main/java/com/book/service/impl/LoginServiceImpl.java
+++ src/main/java/com/book/service/impl/LoginServiceImpl.java
@@ -1,15 +1,23 @@
 package com.book.service.impl;
 
+import cn.hutool.jwt.JWTUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.book.common.BusinessException;
 import com.book.domain.dto.LoginDto;
 import com.book.domain.entity.SysUser;
 import com.book.service.LoginService;
 import com.book.service.SysUserService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class LoginServiceImpl implements LoginService {
+
+    private static final String TOKEN_SECRET = "privateKey";
 
     private SysUserService sysUserService;
 
@@ -22,8 +30,13 @@ public class LoginServiceImpl implements LoginService {
         if(sysUser==null){
             throw new BusinessException("用户不存在或密码错误");
         }
-
-
-        return "";
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        if (!passwordEncoder.matches(dto.getPassword(),sysUser.getPassword())){
+            throw new BusinessException("用户不存在或密码错误");
+        }
+        //如果都检验成功了就可以生成jwt的token了
+        Map<String,Object> map = new HashMap<>();
+        map.put("uid",sysUser.getId());
+        return JWTUtil.createToken(map,TOKEN_SECRET.getBytes(StandardCharsets.UTF_8));
     }
 }
